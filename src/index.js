936aa8b232e40b20dc4fe6c799da6dc79182ec3d
Beyza Arbaz <143319657+beyzarbz@users.noreply.github.com>
2026-08-24 17:25:03 +0000

    Add task assignment behavior

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 console.log('Gastro Ops')
 
-// I have three main objekts ; Branches, Users , Tasks
+// I have three main objekts : Branches, Users , Tasks
 
-// I need to ve able to create a Branch
+// I need to be able to create a Branch
 //User should belong to a Branch
 //A User should have a Role
 
-//WhiCH information shoul my Branch carry ?
+//Which information should my Branch carry ?
+// I need to be able to create Users.
+//Which information should my User carry ?
+// I need to be able to create Tasks.
+//Which information should my Task carry ?
+//I need to be able to assign a Task to a User.
 
 const branch = {
   name: 'Kennys Vienna',
@@ -15,13 +20,16 @@ const branch = {
 
 console.log(`The branch name is ${branch.name} and its address is ${branch.address}.`)
 
-// I need to be able to create Users.
-
 const beyza = {
   name: 'Beyza',
   role: 'branchManager',
   branch: branch,
   tasks: [],
+
+  assignTask(selectedTask, selectedUser) {
+    selectedTask.assignedTo = selectedUser
+    selectedUser.tasks.push(selectedTask)
+  },
 }
 
 const kevin = {
@@ -30,10 +38,6 @@ const kevin = {
   branch: branch,
   tasks: [],
 }
-
-console.log(`${beyza.name} works as ${beyza.role} at ${beyza.branch.name}.`)
-
-console.log(`${kevin.name} works as ${kevin.role} at ${kevin.branch.name} and has ${kevin.tasks.length} tasks.`)
 
 // I need to be able to create Tasks.
 const branchTask = {
@@ -45,15 +49,22 @@ const branchTask = {
   status: 'pending',
   assignedTo: null,
   branch: branch,
-  created_by: beyza,
+  createdBy: beyza,
 }
+beyza.assignTask(branchTask, kevin)
+
+console.log(`${beyza.name} works as ${beyza.role} at ${beyza.branch.name}.`)
+
+console.log(`${kevin.name} works as ${kevin.role} at ${kevin.branch.name} and has ${kevin.tasks.length} tasks.`)
+
+console.log(`${branchTask.title} assigned to ${branchTask.assignedTo.name}.`)
 
 console.log(
-  `${branchTask.title} was created by ${branchTask.created_by.name} for ${branchTask.branch.name} and is currently ${branchTask.status}.`
+  `${branchTask.title} was created by ${branchTask.createdBy.name} for ${branchTask.branch.name} and is currently ${branchTask.status}.`
 )
 
 console.log(
-  `Task status: ${branchTask.status}, Priority: ${branchTask.priority}, Assigned to: ${branchTask.assignedTo}.`
+  `Task status: ${branchTask.status}, Priority: ${branchTask.priority}, Assigned to: ${branchTask.assignedTo.name}.`
 )
 console.log(
   `Task details: ${branchTask.title} - ${branchTask.description}, Start Time: ${branchTask.startTime}, End Time: ${branchTask.endTime}.`
